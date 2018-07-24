170c19d1b61a62b8fc7d4a39b23cede880d7e1e5
Wenqiang Luo <943183575@qq.com>
2018-07-24 23:06:36 +0800

    功能完成

--- src/utils/Logger.java
+++ src/utils/Logger.java
@@ -14,6 +14,10 @@ public class Logger {
 
     private Logger(){}
 
+    /**
+     * 日志记录方法
+     * @param detail 要记录的一条内容
+     */
     public synchronized static void log(String detail){
         try {
             pw = new PrintWriter(new FileOutputStream(DEFAULT_LOG_PATH + LOG_NAME, true));
@@ -26,6 +30,10 @@ public class Logger {
     }
 
 
+    /**
+     * 每条日志内容都默认会加上当前系统时间
+     * @return 当前时间，格式年月日时分秒
+     */
     private static String currentTime(){
         return " " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
     }
